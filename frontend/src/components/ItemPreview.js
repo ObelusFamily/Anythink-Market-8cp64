713ac1775627516cae953b0def53f8c8e729b158
sadiejay <19538219+sadiejay@users.noreply.github.com>
2022-08-12 16:27:08 -0500

    Reformats code

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -39,7 +39,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src= {item.image}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
